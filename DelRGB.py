bbb6cf168160593aa9df4b35c5e760cc7bc53a87
MRATTE <matthias.ratte@gmail.com>
2025-03-11 15:45:06 +0100

    update firmware

--- DelRGB.py
+++ DelRGB.py
@@ -2,25 +2,34 @@ from machine import Pin
 import time
 
 class RGB:
-     def __init__(self, pin_r, pin_g, pin_b):
+    def __init__(self, pin_r, pin_g, pin_b):
         self.r = Pin(pin_r, Pin.OUT)
-        self.r = Pin(pin_g, Pin.OUT)
-        self.r = Pin(pin_b, Pin.OUT)
+        self.g = Pin(pin_g, Pin.OUT)
+        self.b = Pin(pin_b, Pin.OUT)
+         
+         
+    def off(self):
+        self.r.value(0)
+        self.g.value(0)
+        self.b.value(0)
+    
+    def on(self,color):
+        if color=='red':
+            self.r.value(1)
+            self.g.value(0)
+            self.b.value(0)
+        if color=='green':
+            self.r.value(0)
+            self.g.value(1)
+            self.b.value(0)
+        if color=='blue':
+            self.r.value(0)
+            self.g.value(0)
+            self.b.value(1)
 
-def off(self):
-    self.r.value()=0
-    self.g.value()=0
-    self.b.value()=0
-
-def on(self,color):
-    if color==red:
-        self.r.value()=1
-        self.g.value()=0
-        self.b.value()=0
-
-def blink(self,color,delay,number):
-    for i in range(times):
-        self.set_color(r, g, b)
-        time.sleep_ms(delay)
-        self.off()
-        time.sleep_ms(delay)
+    def blink(self,color,delay,number):
+        for i in range(number):
+            self.on(color)
+            time.sleep_ms(delay)
+            self.off()
+            time.sleep_ms(delay)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from ConnectWifi import WiFiConnection
 from parametres import SSID, PASSWORD
 from Temperature import LectureTemperature
 from Affichage import AffichageOled
+from DelRGB import RGB
 from machine import Pin, SoftI2C
 import ssd1306
 import time   
@@ -14,6 +15,7 @@ temp=25
 wifi = WiFiConnection(ssid=SSID, key=PASSWORD, max_retries=10)    #instance de la classe WifiConnection
 capteur_temp = LectureTemperature(i2c,Pin(21),Pin(20))                  #instance de la classe LectureTemperature
 ecran = AffichageOled(128, 64, i2c)                     #instance de la classe AffichageOled
+Led = RGB(2,3,4)
 
 # définition de la fonction interruption bouton
 def interrupt_button(pin):
@@ -28,17 +30,24 @@ def main():
    
     ####connection au wifi#####
     print("Connexion au réseau Wi-Fi...")
+    Led.on('blue')
+    time.sleep(1)
     wlan = wifi.connect()                                     #connexion au réseau
     if wlan.isconnected():
         print("Connecté avec succès!")
-        print("Adresse IP: ", wlan.ifconfig())                  #wlan est utilisable pour d'autres fonctions réseau
+        print("Adresse IP: ", wlan.ifconfig())#wlan est utilisable pour d'autres fonctions réseau
+        Led.on('green')
     else:
         print("Échec de la connexion.")
+        Led.on(red)
 
     while True:
         temp = capteur_temp.readTemp()
         if temp is not None:
+            Led.on('green')
             print(f"Température : {temp} °C")
+        else:
+            Led.on('red')
         time.sleep(5)      
 
 # programme principal.
